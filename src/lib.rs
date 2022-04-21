775ec064f3c9de8872f05b4011ebd1ed665afd9a
Jonathan Brown <jbrown@acuity.social>
2022-04-21 12:03:06 +0700

    Revert "use sp_std::vec::Vec;"
    This reverts commit a61273c223e3b3f9441e3aac910dc073ab78b1fa.

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 /// Learn more about FRAME and the core library of Substrate FRAME pallets:
 /// <https://docs.substrate.io/v3/runtime/frame>
 pub use pallet::*;
-use sp_std::vec::Vec;
+use frame_support::inherent::Vec;
 
 #[cfg(test)]
 mod mock;
